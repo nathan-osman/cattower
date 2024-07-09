0cd06fea51e8d2bd788606180591935fb047a7a9
Nathan Osman <nathan@quickmediasolutions.com>
2024-07-08 22:13:50 -0700

    Add hardware package.

---
+++ hardware/hardware.go
@@ -0,0 +1,19 @@
+//go:build !unix
+
+package hardware
+
+import (
+	"image/color"
+)
+
+// Hardware provides a stub for non-unix platforms
+type Hardware struct {
+}
+
+func New() (*Hardware, error) {
+	return &Hardware{}, nil
+}
+
+func (h *Hardware) SetPixel(i int, c color.Color) {}
+func (h *Hardware) Write() error                  { return nil }
+func (h *Hardware) Close()                        {}
---
+++ hardware/hardware_unix.go
@@ -0,0 +1,44 @@
+//go:build unix
+
+package hardware
+
+import (
+	"image/color"
+
+	"github.com/Jon-Bright/ledctl/pixarray"
+)
+
+// Hardware provides access to the hardware on the system.
+type Hardware struct {
+	ledStrip pixarray.LEDStrip
+}
+
+func New() (*Hardware, error) {
+	l, err := pixarray.NewWS281x(48, 3, pixarray.GRB, 800000, 10, []int{18})
+	if err != nil {
+		return nil, err
+	}
+	return &Hardware{
+		ledStrip: l,
+	}, nil
+}
+
+func (h *Hardware) SetPixel(i int, c color.Color) {
+	r, g, b, _ := c.RGBA()
+	h.ledStrip.SetPixel(
+		i,
+		pixarray.Pixel{
+			R: int(r),
+			G: int(g),
+			B: int(b),
+		},
+	)
+}
+
+func (h *Hardware) Write() error {
+	return h.ledStrip.Write()
+}
+
+func (h *Hardware) Close() {
+	//...
+}
