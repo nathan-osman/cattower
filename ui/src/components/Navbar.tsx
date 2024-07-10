7db932560d2cb5a96b283704012f96ee7ef697c7
Nathan Osman <nathan@quickmediasolutions.com>
2024-07-09 21:40:23 -0700

    Add home button to navbar.

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import Clock from './Clock'
+import HomeSvg from '../images/home.svg'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className="bg-background-inverted text-foreground-inverted shadow-md p-4 flex">
+    <div className="bg-background-inverted text-foreground-inverted shadow-md px-4 py-2 flex items-center">
+      <div className="grow">
+        <Link to="/">
+          <img src={HomeSvg} className="w-8" />
+        </Link>
+      </div>
       <Clock />
     </div>
   )
