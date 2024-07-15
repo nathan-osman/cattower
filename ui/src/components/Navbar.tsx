a36a5b1c679f6b245a5237cb8a6a3d1cd22fa078
Nathan Osman <nathan@quickmediasolutions.com>
2024-07-14 23:21:32 -0700

    Ensure only home icon is link.

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -6,11 +6,10 @@ import HomeSvg from '../images/home.svg'
 export default function Navbar() {
   return (
     <div className="bg-background-inverted text-foreground-inverted shadow-md p-4 flex items-center gap-x-4">
-      <div className="grow">
-        <Link to="/">
-          <img src={HomeSvg} className="w-12" />
-        </Link>
-      </div>
+      <Link to="/">
+        <img src={HomeSvg} className="w-12" />
+      </Link>
+      <div className="grow" />
       <Clock />
       <Volume />
     </div>
